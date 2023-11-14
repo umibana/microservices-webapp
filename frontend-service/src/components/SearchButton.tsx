aaaaaad0edeef966440134cd8acfe9a40a5bc08d
Hans Villarroel <hans.villarroel@usach.cl>
2023-11-14 18:00:22 -0300

    Added Minikube deployment files

--- frontend-service/src/components/SearchButton.tsx
+++ frontend-service/src/components/SearchButton.tsx
@@ -3,30 +3,31 @@ import { Input } from "@/components/ui/input";
 import { useState } from "react";
 
 export function SearchButton() {
-	const [inputValue, setInputValue] = useState("");
-	const [userInfo, setUserInfo] = useState("");
+  const [inputValue, setInputValue] = useState("");
+  const [userInfo, setUserInfo] = useState("");
 
-	const handleInputChange = (event) => {
-		setInputValue(event.target.value);
-	};
+  const handleInputChange = (event: any) => {
+    setInputValue(event.target.value);
+  };
 
-	// Event handler for the button click
-	const handleButtonClick = () => {
-		// We read from the useState and send to over to the microservice to get the user info
-		console.log(inputValue);
-	};
+  // Event handler for the button click
+  const handleButtonClick = () => {
+    // We read from the useState and send to over to the microservice to get the user info
+    console.log(inputValue);
+  };
+  setUserInfo("testing");
 
-	return (
-		<div className="flex w-full max-w-sm items-center space-x-2">
-			<Input
-				type="rut"
-				placeholder="Ingrese Rut del usuario"
-				onChange={handleInputChange}
-			/>
-			<Button type="submit" onClick={handleButtonClick}>
-				Subscribe
-			</Button>
-			{userInfo ? <p>{userInfo}</p> : null}
-		</div>
-	);
+  return (
+    <div className="flex w-full max-w-sm items-center space-x-2">
+      <Input
+        type="rut"
+        placeholder="Ingrese Rut del usuario"
+        onChange={handleInputChange}
+      />
+      <Button type="submit" onClick={handleButtonClick}>
+        Subscribe
+      </Button>
+      {userInfo ? <p>{userInfo}</p> : null}
+    </div>
+  );
 }
--- installment-service/src/main/java/com/example/installmentservice/InstallmentServiceApplication.java
+++ installment-service/src/main/java/com/example/installmentservice/InstallmentServiceApplication.java
@@ -2,8 +2,10 @@ package com.example.installmentservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class InstallmentServiceApplication {
 
 	public static void main(String[] args) {
